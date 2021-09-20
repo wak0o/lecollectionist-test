8627603a16b72629b1c1497dd61beb453ff6d920
Antoine Wachowiak <antoinewachowiak@gmail.com>
2021-09-20 10:24:41 +0200

    feat: Add time range availability validation on OpeningTime

--- app/models/opening_time.rb
+++ app/models/opening_time.rb
@@ -1,7 +1,7 @@
 class OpeningTime < ApplicationRecord
   validates :week_day, :open_at, :close_at, presence: true
   validates :week_day, inclusion: { in: 0..6, message: "%{value} is not a valid day" }
-  validate :opens_before_closes 
+  validate :opens_before_closes, :availability
 
   belongs_to :shop
 
@@ -10,6 +10,10 @@ class OpeningTime < ApplicationRecord
   end
 
   protected
+
+  def availability
+    errors.add(:close_at, "Time range already exist") if OpeningTime.where("open_at <= :a AND :b <= close_at AND shop_id = :c", a: close_at, b: open_at, c: shop.id).exists?
+  end
 
   def opens_before_closes
     errors.add(:close_at, "Open time must be before close time") if open_at && close_at && open_at >= close_at
